cfa1d7f94865c69465639ead3628d79b5899b8f5
leelida <leelida@gmail.com>
2024-06-24 07:59:49 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+
+
+	<?php require_once("top.php");?>
+	<style type="text/css">
+<!--
+.style5 {	font-size: x-large;
+	font-weight: bold;
+	font-family: "標楷體";
+	color: #0000FF;
+}
+-->
+    </style>
+	
+    <div id="body">
+
+		<?php require_once("left.php");?>
+        <div id="content">
+        
+	      <h1><span class="style5">公司簡介</span>
+	      </h1><br />
+	      <h2><strong>芯永科技有限公司成立於2011年8月，設址於台南市東區崇善路826號，為專攻系統整合及軟體代工的新事業。<br />
+        公司的團隊的成員在系統程式及系統整合方面都具有10年以上的經驗，期望系統整合為客戶創造價格合理且良好的優質服務。 </strong><br />
+          <br />
+        <a href="http://www.zymic.com/free-templates/copyright-removal/1133/"></a></h2>
+        <div>
+	      <h1><span class="style5">公司願景</span></h1><br />
+	      <h2><strong>台灣產業已經面臨全球化競爭，進入軟硬體整合及共享的時代,不論是電子任何產業都須將軟體的效益發揮。<br />
+          我們期待能結合過去十年在資訊業領域之系統整合經驗與程式設計實務，與用心而認真的態度，不僅能為客戶『做對的事』，亦能成為客戶所需之希望我們的專業及努力，可以協助客戶的事業推動，達到雙贏的成長目標。</strong></h2>
+	      <h2><br />
+	        <br />
+	        </p>
+	        
+          </h2>
+	    </div>
+	    <h2>&nbsp;</h2>
+      </div>
+        
+        <div class="clear"></div>
+     	<?php require_once("footer.php");?>
+    </div>
+</div>
+</body>
+</html>
